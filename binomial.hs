23f12a9b460557a287b0b6438374e477afa48311
Bosswell <bosswell9@gmail.com>
2022-02-13 12:13:00 +0100

    Improved alg

--- binomial.hs
+++ binomial.hs
@@ -5,10 +5,16 @@ import Data.List
 import Control.Monad
 
 makeNumerator :: Int -> Int -> [Int]
-makeNumerator a b = [a, a - 1 .. a - b + 1]
+makeNumerator a b
+        | a == b = [1]
+        | a - b >= 2 * b = [a, a - 1 .. a - b + 1]
+        | a - b < b = [b + 1 .. a]
 
-makeDenominator :: Int -> [Int]
-makeDenominator b = [1..b]
+makeDenominator :: Int -> Int -> [Int]
+makeDenominator a b
+        | a == b = [1]
+        | a - b >= b = [2 .. b]
+        | a - b < b = [2 .. a - b]
 
 multiplyList :: [Int] -> Int
 multiplyList [] = 1
@@ -30,14 +36,27 @@ readLines = do
         return line
     return lines
 
+makeNumeratorUsingPrimes :: Int -> Int -> [Int]
+makeNumeratorUsingPrimes a = toPrimeFactors . makeNumerator a
+
+makeDenominatorUsingPrimes :: Int -> Int -> [Int]
+makeDenominatorUsingPrimes a = toPrimeFactors . makeDenominator a
+
 getBinomial :: (Int, Int) -> Int
-getBinomial (a, b) = multiplyList (makeNumerator a b) `div` multiplyList (makeDenominator b)
+getBinomial (a, b) = multiplyList $ (makeNumeratorUsingPrimes a b) \\ (makeDenominatorUsingPrimes a b)
+
+primeFactors n =
+  case factors of
+    [] -> [n]
+    _  -> factors ++ primeFactors (n `div` (head factors))
+  where factors = take 1 $ filter (\x -> (n `mod` x) == 0) [2 .. n-1]
+
+toPrimeFactors :: [Int] -> [Int]
+toPrimeFactors [] = []
+toPrimeFactors (x:xs) = primeFactors x ++ toPrimeFactors xs
 
 main :: IO ()
 main =
   do
     lines <- readLines
     mapM_ print $ map getBinomial $ normalizeInput lines
-
-
-
